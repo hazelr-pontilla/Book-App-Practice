a2dec13a97813774baabe4a5e72c01772ba98ed0
hzlpntl <93429158+hzlpntl@users.noreply.github.com>
2023-06-24 23:00:36 +0800

    added code

--- store/models.py
+++ store/models.py
@@ -24,3 +24,6 @@ class Book(models.Model):
     
     def __str__(self):
         return self.title
+    
+    def get_display_price(self):
+        return self.price / 100
--- store/views.py
+++ store/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Book
+
+def book_detail(request, slug):
+    book = Book.objects.get(slug = slug)
+    return render(request,  'store/book_detail.html', {'book': book})
